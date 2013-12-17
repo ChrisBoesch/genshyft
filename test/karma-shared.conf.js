a7162071d459afb5cb0d0259d3b4a06aaaf7a5c2
Ivan Sklyarov <sklyarov.161@gmail.com>
2013-12-17 17:49:49 +0400

    add tests for PlayerController

--- test/karma-shared.conf.js
+++ test/karma-shared.conf.js
@@ -3,20 +3,25 @@ module.exports = function() {
     basePath: '../',
     frameworks: ['mocha'],
     reporters: ['progress'],
-    //browsers: ['Chrome'],
+    // browsers: ['Firefox'],
     browsers: ['PhantomJS'],
     autoWatch: true,
 
     // these are default values anyway
     singleRun: false,
     colors: true,
-    
+    // logLevel: LOG_DEBUG,
     files : [
       //3rd Party Code
-      'bower_components/angular/angular.js',
+      'app/lib/angular/angular.js',
+
+      // 'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angularjs-scope.safeapply/src/Scope.SafeApply.js',
-      'app/scripts/lib/router.js',
+      // 'article-app/scripts/lib/router.js',
+      // 'node_modules/karma-requirejs/lib/require.js',
+      // 'node_modules/chai/lib/chai.js',
+
 
       //App-specific Code
       //'app/scripts/config/config.js',
@@ -27,6 +32,15 @@ module.exports = function() {
       //'app/scripts/config/routes.js',
       //'app/scripts/app.js',
       //'app/js/**/*.js',
+
+    "app/lib/angular/angular-resource.js",
+    "app/js/cookies.js",
+    "app/js/analytics.js",
+    "app/js/ui-ace.js",
+    'app/js/app-config.js',
+
+
+
       'app/js/controllers.js',
       'app/js/app.js',
 
