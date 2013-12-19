b1b56639841c132e152cd58260bcb4e1d43c131f
Rifat Nabi <to.rifat@gmail.com>
2013-12-20 00:55:40 +0600

    fixed e2e testing server verbose output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-open');
@@ -6,10 +6,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-develop');
+  grunt.loadNpmTasks('grunt-verbosity');
 
   grunt.initConfig({
     shell: {
-      options : {
+      options: {
         stdout: true
       },
       npm_install: {
@@ -25,7 +27,7 @@ module.exports = function(grunt) {
 
     connect: {
       options: {
-        base: 'app/'
+        base: '/app'
       },
       webserver: {
         options: {
@@ -61,6 +63,18 @@ module.exports = function(grunt) {
       }
     },
 
+    develop: {
+      server: {
+        file: 'server.js'
+      }
+    },
+
+    verbosity: {
+      server: {
+        tasks: ['karma:e2e']
+      }
+    },
+
     karma: {
       unit: {
         configFile: './test/karma-unit.conf.js',
@@ -90,7 +104,7 @@ module.exports = function(grunt) {
 
     watch: {
       assets: {
-        files: ['app/styles/**/*.css','app/scripts/**/*.js'],
+        files: ['app/styles/**/*.css', 'app/scripts/**/*.js'],
         tasks: ['concat']
       }
     },
@@ -127,19 +141,13 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('test', ['connect:testserver','karma:unit','karma:midway', 'karma:e2e']);
+  grunt.registerTask('test', ['verbosity', 'develop:server', 'karma:unit', 'karma:midway', 'karma:e2e']);
   grunt.registerTask('test:unit', ['karma:unit']);
-  grunt.registerTask('test:midway', ['connect:testserver','karma:midway']);
-  grunt.registerTask('test:e2e', ['connect:testserver', 'karma:e2e']);
-
-  //keeping these around for legacy use
-  grunt.registerTask('autotest', ['autotest:unit']);
-  grunt.registerTask('autotest:unit', ['connect:testserver','karma:unit_auto']);
-  grunt.registerTask('autotest:midway', ['connect:testserver','karma:midway_auto']);
-  grunt.registerTask('autotest:e2e', ['connect:testserver','karma:e2e_auto']);
+  grunt.registerTask('test:midway', ['verbosity', 'develop:server', 'karma:midway']);
+  grunt.registerTask('test:e2e', ['verbosity', 'develop:server', 'karma:e2e']);
 
   //installation-related
-  grunt.registerTask('install', ['shell:npm_install','shell:bower_install','shell:font_awesome_fonts']);
+  grunt.registerTask('install', ['shell:npm_install', 'shell:bower_install', 'shell:font_awesome_fonts']);
 
   //defaults
   grunt.registerTask('default', ['dev']);
