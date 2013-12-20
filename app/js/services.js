47002705fd7ea4161639f620e76adefe2696edac
Rifat Nabi <to.rifat@gmail.com>
2013-12-21 04:40:07 +0600

    fixed default country ranking loading bug

--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,16 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+
+  .factory('currentUser', function () {
+    var _user = {};
+    return {
+      setUser: function (user) {
+        _user = user;
+      },
+      getUser: function () {
+        return _user;
+      }
+    }
+  });
