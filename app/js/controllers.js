b0fcc9297efea6a0ad3d20ef8ca876a8fea03d8e
Damien Lebrun <dinoboff@hotmail.com>
2014-02-11 19:46:51 +0000

    Add ace editor to problem edit form
    Used ui-ace

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4415,7 +4415,7 @@ function EditProblemController($scope, $http, $q, $window) {
                 examples: $scope.problemDetails.examples,
                 tests: $scope.problemDetails.privateTests || ""
             };
-        console.log('foo');
+
         $scope.resetTestRun();
         $http.post('/jsonapi/check_code_with_interface', publicData, postConfig).then(function(resp) {
             $scope.testRun = resp.data;
