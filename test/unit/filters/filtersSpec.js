431e3195352acc4c3aa1113fdb5a87a43ae68780
Damien Lebrun <dinoboff@hotmail.com>
2014-02-18 11:59:34 +0000

    Fix renderMs filter to round ms values

--- test/unit/filters/filtersSpec.js
+++ test/unit/filters/filtersSpec.js
@@ -2,41 +2,37 @@
 // test/unit/filters/filtersSpec.js
 //
 describe("Unit: Testing Filters", function() {
-/*
-  beforeEach(angular.mock.module('App'));
+    var filter;
 
-  it('should have a range filter', inject(function($filter) {
-    expect($filter('range')).not.to.equal(null);
-  }));
+    beforeEach(angular.mock.module('myApp.filters'));
 
-  it('should have a range filter that produces an array of numbers', inject(function($filter) {
-    var range = $filter('range')([], 5);
-    expect(range.length).to.equal(5);
-    expect(range[0]).to.equal(0);
-    expect(range[1]).to.equal(1);
-    expect(range[2]).to.equal(2);
-    expect(range[3]).to.equal(3);
-    expect(range[4]).to.equal(4);
-  }));
+    beforeEach(inject(function($filter) {
+        filter = $filter;
+    }));
 
-  it('should return null when nothing is set', inject(function($filter) {
-    var range = $filter('range')();
-    expect(range).to.equal(null);
-  }));
+    it('should have a renderMs filter', function() {
+        expect(filter('renderMs')).toBeTruthy();
+    });
 
-  it('should return the input when no number is set', inject(function($filter) {
-    var range, input = [1];
-    range = $filter('range')(input);
-    expect(range).to.equal(input);
+    describe('renderMs', function() {
+        var renderMs;
 
-    range = $filter('range')(input, 0);
-    expect(range).to.equal(input);
+        beforeEach(function() {
+            renderMs = filter('renderMs');
+        });
 
-    range = $filter('range')(input, -1);
-    expect(range).to.equal(input);
+        it('should render small time values in ms', function() {
+            expect(renderMs(999)).toBe('999ms');
+        });
 
-    range = $filter('range')(input, 'Abc');
-    expect(range).to.equal(input);
-  }));
-*/
+        it('should render time values greater than 1s in s', function() {
+            expect(renderMs(1550)).toBe('1.55s');
+        });
+
+        it('should round values', function() {
+            expect(renderMs(1000/3)).toBe('333ms');
+            expect(renderMs(10000/3)).toBe('3.33s');
+        });
+    });
+
 });
