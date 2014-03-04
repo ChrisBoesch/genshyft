1fbefd075acd5a63be82f62b63d0b02e6ad24107
Chris Boesch <cboesch@smu.edu.sg>
2014-03-04 06:31:25 +0000

    Removing files from grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -205,29 +205,7 @@ module.exports = function (grunt) {
           'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
           'bower_components/angular-ui-ace/ui-ace.js',
           'bower_components/angular-leaflet-directive/src/angular-leaflet-directive.js',
-          'bower_components/bootstrap-css/js/bootstrap.js',
-          'app/js/angular-google-maps.js',
-          'app/js/app-config.js',
-          'app/js/app.js',
-          'app/js/admin-controllers.js',
-          'app/js/analytics.js',
-          'app/js/BATController.js',
-          'app/js/controllers.js',
-          'app/js/dir-dnd.js',
-          'app/js/directives.js',
-          'app/js/fancybox-fire.js',
-          'app/js/filters.js',
-          'app/js/game-play-app.js',
-          'app/js/mbcoaching-controllers.js',
-          'app/js/PurposeDrivenAdminController.js',
-          'app/js/PurposeDrivenController.js',
-          'app/js/school-controllers.js',
-          'app/js/services.js',
-          'app/js/tournament-controllers.js',
-          'app/js/TournamentGameController.js',
-          'app/js/ymbcoaching-controllers.js',
-          'app/js/ymbcoaching-play-controllers.js',
-          'app/js/WebGameController.js'
+          'bower_components/bootstrap-css/js/bootstrap.js'
         ]
       }
     }
