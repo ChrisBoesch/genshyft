e785938965d2e438220069855dc5626f027bc029
glenwks <glen.wong.2011@smu.edu.sg>
2014-03-22 20:07:43 +0800

    added fetch ranking with time

--- app/js/tournament-controllers.js
+++ app/js/tournament-controllers.js
@@ -110,6 +110,44 @@ function GenshyftTournamentController($scope,$resource,$timeout,$location,$cooki
     } 
   };
 
+  /* Fetch ranking with time - by Glen GENShYFT*/
+  $scope.fetch_ranks_with_time = function(heatID,hour,minute,seconds){
+    console.log("Time: " + hour+":"+minute+":"+seconds+".0000");
+    if(!isNaN(hour)&&!isNaN(minute)&&!isNaN(seconds)){
+      if(hour.toString().length==1){
+        hour = "0"+hour.toString();
+      }else if(hour.toString().length==0){
+        hour = "00";
+      }
+
+     if(minute.toString().length==1){
+        minute = "0"+minute.toString();
+      }else if(minute.toString().length==0){
+        minute = "00";
+      } 
+
+      if(seconds.toString().length==1){
+        seconds = "0"+seconds.toString();
+      }else if(seconds.toString().length==0){
+        seconds = "00";
+      } 
+
+      var time = hour+":"+minute+":"+seconds+".0000"
+      console.log("Fetching rankings at " +time+" ...");
+      $scope.GHeatModel.get({"heatID":heatID,"nocache":"True", "time":time}, function(response){
+        $scope.tournament = response;
+        $scope.playerRanks = $scope.tournament.ranking;        
+      });
+      console.log("Rankings at "+ time +" fetched.");
+    }else if(hour==undefined||minute==undefined||seconds==undefined){
+      alert("Please fill in all fields.");
+    }else{
+      alert("Invalid values, only numbers allowed.");
+    }
+  };
+
+
+
   $scope.my_range = function(n) {
     var result = [];
     var counter = 1;
